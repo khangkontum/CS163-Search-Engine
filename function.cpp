2506489a9e9ad283969858183f7a3f3e8a662dcf
dvtngan <dvtngan@gmail.com>
2021-07-07 13:28:14 +0700

    upload trie and stopwords database

--- function.cpp
+++ function.cpp
@@ -1,62 +1,54 @@
 #include"Header.h"
-void Trie::insert(string word,string fileName) {
+string wordIgnore(string t) {
+	string result = "";
+	for (int i = 0; i < t.length(); i++) {
+		if (int(t[i] - 33) * (int(t[i]) - 127) <= 0)
+			result += t[i];
+	}
+	return result;
+}
+void Trie::insert(string word, string fileName) {
 	Trie* pCur = this;
 	for (int i = 0; i < word.length(); ++i) {
-		if(!pCur->child[word[i] - ' '])
+		if (!pCur->child[word[i] - ' '])
 			pCur->child[word[i] - ' '] = new Trie();
 		pCur = pCur->child[word[i] - ' '];
 	}
-	pCur->fileName.insert(fileName);
+	pCur->fileName.push_front(fileName);
 	pCur->cnt++;
 }
 
-bool Trie::isExist(string word) {
-	Trie* pCur = this;
-	for (int i = 0; i < word.length(); i++) {
-		if (!pCur->child[word[i] - ' '])
-			return false;
-		pCur = pCur->child[word[i] - ' '];
-	}
-	return true;
-}
-
 bool loadData(Trie* dataRoot, Trie* stopwordsRoot, Trie* thesaurusRoot) {
-	// load stopword
-	ifstream Read("Database/Stopwords/stopwords.txt");
-	string stopword;
-	while(Read >> stopword) {
-		stopwordsRoot->insert(stopword, "");
-	}
 
 	// load data
-	ifstream fin("Database/Search Engine-Data/___index.txt");
+	ifstream fin("Search Engine-Data/___index.txt");
 	if (!fin.is_open())
 		return false;
 	while (!fin.eof()) {
-		string fileName; 
-		getline(fin,fileName,'\n');
-		ifstream fin1("Database/Search Engine-Data/" + fileName);
+		string fileName; getline(fin, fileName, '\n');
+		ifstream fin1("Search Engine-Data/" + fileName);
 		if (!fin1.is_open())
 			return false;
+		cout << fileName << endl;
 		while (!fin1.eof()) {
-			string word; 
-			fin1 >> word;
-			if (stopwordsRoot->isExist(word))	//If word is stopword then ignore
-				continue;
-			dataRoot->insert(word,fileName);
+			string word; fin1 >> word;
+			dataRoot->insert(wordIgnore(word), fileName);
 		}
 	}
-	fin.close();
 
-	
+
+	// load stopwords
 
 	// load thesaurus
 
 
 	return true;
 }
+bool isStopWords(Trie* stopWordsTrie, string s) {
+	return true;
+}
 
-void function_1(Trie* root){
+void function_1(Trie* root) {
 
 }
 void function_2(Trie* root) {
