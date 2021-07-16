4201ca7f221be4d13b53119ebbcc4037b42d13e1
vohuynh19 <vohuynh01092002@gmail.com>
2021-07-16 08:42:43 +0700

    function 1+2+3

--- Header.h
+++ Header.h
@@ -5,17 +5,17 @@
 using namespace std;
 // structure
 struct Trie {
-	Trie* child[96];  // from "33" to "128"
-	map<string,	vector<int> > fileArr;
-	int cnt;
+	Trie* child[38];  // a->z : 0->25 , 0->9 : 26->35 , # $ : 36->37
+	map<string, vector<int> > fileArr;
 	Trie() {
-		for (int i = 0; i < 96; ++i)
+		for (int i = 0; i < 38; ++i)
 			child[i] = nullptr;
-		cnt = 0;
 	}
 	void insert(string word, string fileName, int pos);
 	int wordInFile(string word, string fileName);
 };
+
+
 extern int totalWord;
 extern Trie* dataRoot;
 extern Trie* stopwordsRoot;
@@ -26,6 +26,7 @@ extern map<string, int> maxwd;
 // input function
 pair<int, string> getInput();
 // load function
+int letterToInt(char a);
 string wordIgnore(string t);
 bool loadData();
 
@@ -37,18 +38,21 @@ double tfidf(string word, string fileName);
 Trie* getLeaf(string word);
 void rankAndDisplay(map<string, double> score);
 map<string, double> function_1(string doc);
-
-void function_2(Trie* root);
+//function 2
+map<string, double> function_2(string doc);
 // function 3
 map<string, double> function_3(string doc);
 // function 4
-string upperDocument(string doc);
 map<string, double> function_4(string doc);
 
 void function_5(Trie* root);
 void function_6(Trie* root);
-void function_7(Trie* root);
-void function_8(Trie* root);
+
+// function 7
+map<string, double> function_7(string doc);
+// function 8
+map<string, double> function_8(string doc);
+
 void function_9(Trie* root);
 void function_10(Trie* root);
 void function_11(Trie* root);
